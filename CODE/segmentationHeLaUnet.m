4fce279ccd9363d7ddbdbebbb57bef5039743fed
Constantino Carlos Reyes-Aldasoro <35769703+reyesaldasoro@users.noreply.github.com>
2020-05-15 16:12:10 +0100

    Run with 64x64 training data

--- CODE/segmentationHeLaUnet.m
+++ CODE/segmentationHeLaUnet.m
@@ -45,7 +45,7 @@ end
 % as with the classNames. For randen examples, these vary 1-5, 1-16, 1-10
 labelIDs                    = (1:numClasses);
 pxds                        = pixelLabelDatastore(labelDir,classNames,labelIDs);
-for numEpochsName=1 %:4
+for numEpochsName=1 :4
     switch numEpochsName
         case 1
             numEpochs       = 10;
@@ -58,7 +58,7 @@ for numEpochsName=1 %:4
     end
     
     % try with different encoders
-    for caseEncoder =2 %1:3
+    for caseEncoder =1:3
         switch caseEncoder
             case 1
                 typeEncoder     = 'sgdm';
@@ -75,7 +75,7 @@ for numEpochsName=1 %:4
         numFilters                  = 64;
         filterSize                  = 3;
         
-        for numLayersNetwork =1 %:3
+        for numLayersNetwork =1:3
             switch numLayersNetwork
                 case 1
                     layers = [
@@ -192,3 +192,5 @@ end
 
 
 misclassification = 100*(1-accuracy);
+
+
