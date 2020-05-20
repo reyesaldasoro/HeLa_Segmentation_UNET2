7a765d38fc085fb1f28e7e233f2b951bad92f063
Constantino Carlos Reyes-Aldasoro <35769703+reyesaldasoro@users.noreply.github.com>
2020-05-20 11:42:24 +0100

    Update PrepareTrainingData_4Classes.m
    Ground truth for one cell 300 slices with 4 classes

--- CODE/PrepareTrainingData_4Classes.m
+++ CODE/PrepareTrainingData_4Classes.m
@@ -24,39 +24,59 @@ for currentSlice        = 101:2:180
     currentData         = imread(strcat('D:\OneDrive - City, University of London\Acad\AlanTuringStudyGroup\Crick_Data\ROI_1656-6756-329\ROI_1656-6756-329_z0',num2str(currentSlice),'.tiff'));
     currentSeg          = imread(strcat('D:\OneDrive - City, University of London\Acad\AlanTuringStudyGroup\Crick_Data\ROI_1656-6756-329_manual\ROI_1656-6756-329_z0',num2str(currentSlice),'.tif'));
     % Calculate the envelope and its centreline
-    currentData         = imfilter(currentData,[ 0.0625    0.1250    0.0625; 0.1250    0.2500    0.1250; 0.0625    0.1250    0.0625]);
+    currentData         = imfilter(currentData,[ 0.0625    0.1250    0.0625; 0.1250    0.2500    0.1250; 0.0625    0.1250    0.0625],'replicate');
+    
+    [rows,cols]         = size(currentData);
+    
+    [Hela_background,Background_intensity,Hela_intensity,Hela_output]           = segmentBackgroundHelaEM(currentData);
     nuclearEnvelope     = imdilate(currentSeg>0,ones(12));
     nuclearEnvelopeLin  = bwmorph(nuclearEnvelope,'thin','inf');
-    % Create an index of random points along the nuclear envelope
-    [r,c]               = find(nuclearEnvelopeLin);
-    numPoints           = numel(r);
-    numRandom           = round(numPoints/10);
-    indexRand           = sort(1+round(numPoints*rand(numRandom,1)));
     % Create the classes to be used for training
-    currentRegions      = bwlabel(nuclearEnvelope==0);
+    % This is for Nucleus/Nuclear Envelope/Else
+    currentRegions1      = bwlabel(nuclearEnvelope==0);
+    % This is for Nucleus/Nuclear Envelope/Cell/Background
+    currentRegions      = 1 * (nuclearEnvelope==1)+...                      % NE
+                          2 * (currentRegions1==2  )+...                    % nucleus
+                          3 * ((currentRegions1==1)-Hela_background)+...    % cell
+                          4 * (Hela_background );                           % background
+                          
     currentRegions_R    = regionprops(currentRegions,'Area','boundingbox');
     %
     % Classes:
-    % 0 - nuclear envelope
-    % 1 - The background is always 1 as it touches the edges of the image,
-    % 2 - nucleus is 2 and up
-    currentRegions(currentRegions>2)    = 2;
-    % add one to have labels from 1
-    currentRegions      = currentRegions+1;
-    % Iterate over the random points and save
-    for cTrainReg = 1:numel(indexRand)
-        % Generate the training data and labels
-        try
-        trainingRegionRows  = r(indexRand(cTrainReg))-(sizeTrainingPatch/2)+1:r(indexRand(cTrainReg))+sizeTrainingPatch/2;
-        trainingRegionCols  = c(indexRand(cTrainReg))-(sizeTrainingPatch/2)+1:c(indexRand(cTrainReg))+sizeTrainingPatch/2;
-        currentLabel        = uint8(currentRegions(trainingRegionRows,trainingRegionCols));
-        currentSection      = currentData(trainingRegionRows,trainingRegionCols);
-        % Save training data and labels
-        fName               = strcat('Hela_Data_Slice_',num2str(currentSlice),'_Sample_',num2str(cTrainReg),'.png');
-        fNameL              = strcat('Hela_Label_Slice',num2str(currentSlice),'_Sample_',num2str(cTrainReg),'.png');
-        imwrite(currentSection,strcat('trainingImages',filesep,fName))
-        imwrite(currentLabel,strcat('trainingLabels',filesep,fNameL))
-        catch
+    % 1 - nuclear envelope
+    % 2 - Nucleus
+    % 3 - The cell
+    % 4 - The background is always 1 as it touches the edges of the image,
+
+    currentRegions(currentRegions>4)    = 4;
+
+    % Strategy 1
+    % Iterate over the random points over boundaries and save
+    % Create an index of random points along the nuclear envelope
+    %[r,c]               = find(nuclearEnvelopeLin);
+    % numPoints           = numel(r);
+    % numRandom           = round(numPoints/10);
+    % indexRand           = sort(1+round(numPoints*rand(numRandom,1)));
+    %for cTrainReg = 1:numel(indexRand)
+    %    try
+    %    trainingRegionRows  = r(indexRand(cTrainReg))-(sizeTrainingPatch/2)+1:r(indexRand(cTrainReg))+sizeTrainingPatch/2;
+    %    trainingRegionCols  = c(indexRand(cTrainReg))-(sizeTrainingPatch/2)+1:c(indexRand(cTrainReg))+sizeTrainingPatch/2;
+    
+    % Strategy 2
+    % Iterate over the the whole region and save
+    for counterR = 1:rows-sizeTrainingPatch
+        trainingRegionRows  = counterR:counterR+sizeTrainingPatch-1 ;
+        for counterC = 1:cols-sizeTrainingPatch
+            trainingRegionCols  = counterC:counterC+sizeTrainingPatch-1 ;
+            
+            % Generate the training data and labels
+            currentLabel        = uint8(currentRegions(trainingRegionRows,trainingRegionCols));
+            currentSection      = currentData(trainingRegionRows,trainingRegionCols);
+            % Save training data and labels
+            fName               = strcat('Hela_Data_Slice_',num2str(currentSlice),'_Sample_',num2str(counterR),'_',num2str(counterC),'.png');
+            fNameL              = strcat('Hela_Label_Slice',num2str(currentSlice),'_Sample_',num2str(counterR),'_',num2str(counterC),'.png');
+            imwrite(currentSection,strcat('trainingImages_4c',filesep,fName))
+            imwrite(currentLabel,strcat('trainingLabels_4c',filesep,fNameL))
         end
         %
     end
