1d418e92a2e1a6fd2ed048a5446998feb67324d3
Constantino Carlos Reyes-Aldasoro <35769703+reyesaldasoro@users.noreply.github.com>
2021-08-20 15:11:29 +0100

    Update PrepareTrainingData_multiNuclei.m

--- CODE/PrepareTrainingData_multiNuclei.m
+++ CODE/PrepareTrainingData_multiNuclei.m
@@ -6,7 +6,7 @@ dirGT   = dir(strcat(baseDir_GT,'*.mat'));
 
 scrsz=get(0,'screensize');
 %%
-k=149;
+k=43;
 k1=min(k+10,300);
 k2=max(k-10,1);
 currData    = imfilter(imread(strcat(baseDir_HelaCell,dirHela(k).name)),fspecial('Gaussian',3,1));
@@ -33,7 +33,7 @@ h31=gca;
 imagesc(currDataup)
 colormap gray
 zoom on
-h1.Position=[325 40 880 730];
+h1.Position=[225 40 1080 730];
 h2.Position=[1     450 320 330];
 h3.Position=[1210  450 320 330];
 h11.Position=[0.01 0.01 0.98 0.98];
@@ -46,7 +46,7 @@ groundTruth2=groundTruth;
 figure(1)
 addRegion   = roipoly();
 %groundTruth2=groundTruth2-currAdd*(addRegion);
-groundTruth2(addRegion)=2;
+groundTruth2(addRegion>0)=2;
 currClasses(:,:,1) = currData+uint8(groundTruth2==2)*100;
 currClasses(:,:,2) = currData+uint8(groundTruth2==4)*30;
 currClasses(:,:,3) = currData+uint8(groundTruth2==3)*60;
