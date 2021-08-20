905beda19a0c2f37537851ecd484c2876851ecca
Constantino Carlos Reyes-Aldasoro <35769703+reyesaldasoro@users.noreply.github.com>
2021-08-20 16:51:09 +0100

    Update PrepareTrainingData_multiNuclei.m

--- CODE/PrepareTrainingData_multiNuclei.m
+++ CODE/PrepareTrainingData_multiNuclei.m
@@ -6,54 +6,36 @@ dirGT   = dir(strcat(baseDir_GT,'*.mat'));
 
 scrsz=get(0,'screensize');
 %%
-k=43;
-k1=min(k+10,300);
-k2=max(k-10,1);
+k=195;
+k1=min(k+10,300);k2=max(k-10,1);
 currData    = imfilter(imread(strcat(baseDir_HelaCell,dirHela(k).name)),fspecial('Gaussian',3,1));
 currDataup  = imfilter(imread(strcat(baseDir_HelaCell,dirHela(k1).name)),fspecial('Gaussian',3,1));
 currDatadw  = imfilter(imread(strcat(baseDir_HelaCell,dirHela(k2).name)),fspecial('Gaussian',3,1));
-
 load(strcat(baseDir_GT,dirGT(k).name),'groundTruth');
 currClasses(:,:,1) = currData+uint8(groundTruth==2)*100;
 currClasses(:,:,2) = currData+uint8(groundTruth==4)*30;
 currClasses(:,:,3) = currData+uint8(groundTruth==3)*60;
 currClasses(currClasses>255)=255;
-h1=figure(1);
-h11=gca;
-imagesc(currClasses)
-grid on
-zoom on
-h2=figure(2);
-h21=gca;
-imagesc(currDatadw)
-colormap gray
-zoom on
-h3=figure(3);
-h31=gca;
-imagesc(currDataup)
-colormap gray
-zoom on
-h1.Position=[225 40 1080 730];
-h2.Position=[1     450 320 330];
-h3.Position=[1210  450 320 330];
-h11.Position=[0.01 0.01 0.98 0.98];
-h21.Position=[0 0 1 1];
-h31.Position=[0 0 1 1];
+h1=figure(1);h11=gca;imagesc(currClasses)
+grid on;zoom on
+h2=figure(2);h21=gca;imagesc(currDatadw)
+colormap gray;zoom on
+h3=figure(3);h31=gca;imagesc(currDataup)
+colormap gray;zoom on
+h1.Position=[225 40 1080 730];h2.Position=[1     450 320 330];h3.Position=[1210  450 320 330];
+h11.Position=[0.01 0.01 0.95 0.93];h21.Position=[0 0 1 1];h31.Position=[0 0 1 1];
 groundTruth2=groundTruth;
 %groundTruth3=groundTruth;
-%%
-
-figure(1)
-addRegion   = roipoly();
+%
+%figure(1);addRegion   = roipoly();
 %groundTruth2=groundTruth2-currAdd*(addRegion);
-groundTruth2(addRegion>0)=2;
-currClasses(:,:,1) = currData+uint8(groundTruth2==2)*100;
-currClasses(:,:,2) = currData+uint8(groundTruth2==4)*30;
-currClasses(:,:,3) = currData+uint8(groundTruth2==3)*60;
-currClasses(currClasses>255)=255;
-imagesc(currClasses)
-%%
-
+%groundTruth2(addRegion>0)=2;
+%currClasses(:,:,1) = currData+uint8(groundTruth2==2)*100;currClasses(:,:,2) = currData+uint8(groundTruth2==4)*30;
+%currClasses(:,:,3) = currData+uint8(groundTruth2==3)*60;currClasses(currClasses>255)=255;
+%imagesc(currClasses)
+ %
 groundTruth = groundTruth2;
 filename    = strcat('GroundTruth_multiNuclei',filesep,'GT_Slice_',num2str(k,'%03d'),'.mat');
 save(filename,'groundTruth');
+%filename    = strcat('GroundTruth_multiNuclei',filesep,'GT_Slice_',num2str(k+1,'%03d'),'.mat');
+%save(filename,'groundTruth');
