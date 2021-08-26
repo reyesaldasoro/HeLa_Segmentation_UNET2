1a6e51998e0eaea6e249f1f64dbaf9587698c4e3
Constantino Carlos Reyes-Aldasoro <35769703+reyesaldasoro@users.noreply.github.com>
2021-08-26 10:31:05 +0100

    Update HelaUnetResults_2GT.m

--- CODE/HelaUnetResults_2GT.m
+++ CODE/HelaUnetResults_2GT.m
@@ -116,7 +116,54 @@ end
 % figure(3)
 % imagesc((resultAll(:,:,1)==2)+)
 
-%%
+%%  Recover data from Figure in PLOS Paper
+
+
+%
+uiopen('C:\Users\sbbk034\OneDrive - City, University of London\Documents\GitHub\HeLa-Cell-Segmentation\MatlabFigs\PLOS_Fig7_latest.fig',1)
+
+h0=gcf;
+% get the axes
+h1=h0.Children(1);
+h2=h0.Children(2);
+h3=h0.Children(4);
+
+% get values
+x1=h1.Children(1).XData;
+a_inc=h1.Children(1).YData;
+a_res=h1.Children(2).YData;
+a_vgg=h1.Children(3).YData;
+a_ImPr=h1.Children(4).YData;
+x2=h2.Children(1).XData;
+j_inc=h2.Children(1).YData;
+j_res=h2.Children(2).YData;
+j_vgg=h2.Children(3).YData;
+j_ImPr=h2.Children(4).YData;
+close(figure(1))
+%
+%x1=1:numel(a1);
+%x2=1:numel(j1);
+figure(2)
+hold on
+hp1=plot(x1,a_inc,'b--','linewidth',2);
+hp2=plot(x1,a_res,'r-');
+hp3=plot(x1,a_vgg,'-','color',[1 1 1]*0.4);
+hp4=plot(x1,a_ImPr,'k','linewidth',2);
+hold off
+close(figure(2))
+%
+figure(3)
+%plot(x2,j1,x2,j2,x2,j3,x2,j4)
+hold on
+hp1=plot(x2,j_inc,'b--','linewidth',2);
+hp2=plot(x2,j_res,'r-');
+hp3=plot(x2,j_vgg,'-','color',[1 1 1]*0.4);
+hp4=plot(x2,j_ImPr,'k','linewidth',2);
+hold off
+
+close(figure(3))
+
+
 
 %%
 h1 =figure(1);
@@ -141,28 +188,57 @@ hp2=plot(1:300,jaccard1,1:300,jaccard2,1:300,jaccard3,'linewidth',2);
 h12.YTick =0:0.25:1; h12.XLim=[20 266];
 title('jaccard')
 grid on
-%
-h2 =figure(2);
-h21=subplot(121);
-hp1=plot(1:300, accuracy1, 1:300, accuracy2,1:300, accuracy3,'linewidth',2);
- hp1(2).Color='r';
-  hp1(1).Color=[0 0.56448 1];
-   hp1(3).Color='k';
+%%
+h7 =figure(7);
+%h21=subplot(121);
+h111=gca;
+hold on
+hp11=plot(1:300, accuracy1,'linewidth',2,'color',[0 0.56448 1]);
+hp12=plot(1:300, accuracy2,'linewidth',1,'color',[1 0 1]);
+hp13=plot(1:300, accuracy3,'linewidth',3,'color',[0 0.6 0]);
+hp1=plot(x1,a_inc,'b--','linewidth',0.5);
+hp2=plot(x1,a_res,'r-');
+hp3=plot(x1,a_vgg,'-','color',[1 1 1]*0.4);
+hp4=plot(x1,a_ImPr,'k','linewidth',3);
+hold off
+% hp1(2).Color='r';
+%  hp1(1).Color=[0 0.56448 1];
+%   hp1(3).Color='k';
 
-h21.XLim=[1 300]; h21.YLim=[0.76 1];
-h21.YTick =0.76:0.04:1;
+h111.XLim=[1 300]; h111.YLim=[0.84 1];
+h111.YTick =0.84:0.04:1;
 grid on
 title('accuracy')
-h22=subplot(122);
-hp2=plot(1:300,jaccard1,1:300,jaccard2,1:300,jaccard3,'linewidth',2);
- hp2(2).Color='r';
-  hp2(1).Color=[0 0.56448 1];
- hp2(3).Color='k';
+h7.Position =[ 15         400        1000          300];
+
+h111.Position=[0.04 0.09 0.94 0.82];
+
+
+%%
+h8 =figure(8);
+h222=gca;
+%h22=subplot(122);
+hold on
+hp21=plot(1:300,jaccard1,'linewidth',2,'color',[0 0.56448 1]);
+hp22=plot(1:300,jaccard2,'linewidth',1,'color',[1 0 1]);
+hp23=plot(1:300,jaccard3,'linewidth',3,'color',[0 0.6 0]);
+hp24=plot(x2,j_inc,'b--','linewidth',0.5);
+hp25=plot(x2,j_res,'r-');
+hp26=plot(x2,j_vgg,'-','color',[1 1 1]*0.4);
+hp27=plot(x2,j_ImPr,'k','linewidth',2);
+hold off
+%  hp2(2).Color='r';
+%   hp2(1).Color=[0 0.56448 1];
+%  hp2(3).Color='k';
   
 h22.XLim=[1 300];h22.YLim=[0 1];
  h22.YTick =0:0.25:1;
 title('jaccard')
 grid on
+
+h8.Position =[ 15         100        1000          300];
+
+h222.Position=[0.04 0.09 0.94 0.82];
 %%
 h4=figure(4);
 h41=gca;
