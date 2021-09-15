71c10d1508269913c8d20ce84bef27e584cc6093
Constantino Carlos Reyes-Aldasoro <35769703+reyesaldasoro@users.noreply.github.com>
2021-09-15 16:59:24 +0100

    Update PrepareTrainingData_multiNuclei.m

--- CODE/PrepareTrainingData_multiNuclei.m
+++ CODE/PrepareTrainingData_multiNuclei.m
@@ -1,5 +1,9 @@
+clear variables
+close all
+%%
 baseDir_HelaCell ='C:\Users\sbbk034\OneDrive - City, University of London\Acad\AlanTuringStudyGroup\Crick_Data\ROIS\ROI_1656-6756-329\';
-baseDir_GT       ='C:\Users\sbbk034\OneDrive - City, University of London\Documents\GitHub\HeLa_Segmentation_UNET\CODE\GroundTruth_4c\';
+%baseDir_GT       ='C:\Users\sbbk034\OneDrive - City, University of London\Documents\GitHub\HeLa_Segmentation_UNET\CODE\GroundTruth_4c\';
+baseDir_GT       ='C:\Users\sbbk034\OneDrive - City, University of London\Documents\GitHub\HeLa_Segmentation_UNET2\CODE\GroundTruth_multiNuclei\';
                  
 dirHela = dir(strcat(baseDir_HelaCell,'*.tiff'));
 dirGT   = dir(strcat(baseDir_GT,'*.mat'));
@@ -7,7 +11,7 @@ dirGT   = dir(strcat(baseDir_GT,'*.mat'));
 
 scrsz=get(0,'screensize');
 %%
-k=253;      k1=min(k+5,300);k2=max(k-5,1);
+k=13;      k1=min(k+5,300);k2=max(k-5,1);
 currData    = imfilter(imread(strcat(baseDir_HelaCell,dirHela(k).name)),fspecial('Gaussian',3,1));
 currDataup  = imfilter(imread(strcat(baseDir_HelaCell,dirHela(k1).name)),fspecial('Gaussian',3,1));
 currDatadw  = imfilter(imread(strcat(baseDir_HelaCell,dirHela(k2).name)),fspecial('Gaussian',3,1));
@@ -19,9 +23,12 @@ currClasses(currClasses>255)=255;
 h1=figure(1);h11=gca;imagesc(currClasses);grid on;zoom on
 h2=figure(2);h21=gca;imagesc(currDatadw);colormap gray;zoom on
 h3=figure(3);h31=gca;imagesc(currDataup);colormap gray;zoom on
-h1.Position=[225 40 1080 730];h2.Position=[1     450 320 330];h3.Position=[1210  450 320 330];
+h1.Position=[300 40 900 730];h2.Position=[1     450 320 330];h3.Position=[1210  450 320 330];
 h11.Position=[0.03 0.03 0.95 0.95];h21.Position=[0 0 1 1];h31.Position=[0 0 1 1];
 groundTruth2=groundTruth;
+%h11.XLim = [0 800];
+%h11.YLim = [0 600];
+%axis([0 800 0 600])
 %%
 figure(1);addRegion   = roipoly();
 %groundTruth2=groundTruth2-currAdd*(addRegion);
@@ -33,5 +40,5 @@ imagesc(currClasses)
 groundTruth = groundTruth2;
 filename    = strcat('GroundTruth_multiNuclei',filesep,'GT_Slice_',num2str(k,'%03d'),'.mat');
 save(filename,'groundTruth');
-filename    = strcat('GroundTruth_multiNuclei',filesep,'GT_Slice_',num2str(k+1,'%03d'),'.mat');
-save(filename,'groundTruth');
+%filename    = strcat('GroundTruth_multiNuclei',filesep,'GT_Slice_',num2str(k+1,'%03d'),'.mat');
+%save(filename,'groundTruth');
