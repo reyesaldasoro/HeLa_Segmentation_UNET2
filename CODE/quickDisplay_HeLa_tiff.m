fbf9c21ff29fa8c3f230a2ab033b58e7fc1b3a70
Constantino Carlos Reyes-Aldasoro <35769703+reyesaldasoro@users.noreply.github.com>
2023-07-06 12:00:38 +0100

    Update quickDisplay_HeLa_tiff.m

--- CODE/quickDisplay_HeLa_tiff.m
+++ CODE/quickDisplay_HeLa_tiff.m
@@ -4,11 +4,11 @@
     baseDirData     = 'C:\Users\sbbk034\Documents\Acad\Crick\Hela8000_tiff\';
     dirData         = dir(strcat(baseDirData,'*.tiff'));
 %%
-currentSlice = 20;
+currentSlice = 281;
 currentData         = imread(strcat(baseDirData,dirData(currentSlice).name));
 dataF               = imfilter(currentData,gaussF(3,3,1),'replicate');
 imagesc(dataF)
 
-axis([1 3500 3000 6000 ])
+%axis([1 3500 3000 6000 ])
 
 % filename =strcat('Hela_slice_',num2str(currentSlice),'_StrangeCells_A_B.png');
